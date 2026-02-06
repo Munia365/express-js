777803e8ea83d07489b018ea67c22ce6d0e1dabd
Munia365 <muniasultana6826@gmail.com>
2026-02-06 20:42:06 +0600

    updated

--- routes/app.js
+++ routes/app.js
@@ -2,22 +2,14 @@ const express = require('express');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   console.log(req.url, req.method);
-//   next();
-// });
+const userRouter = require('./user');
 
-app.get('/', (req, res, next) => {
-  console.log("Handling home page request", req.url, req.method);
-  res.send(
-    `<h1> Welcome to airbnb </h1>
-    <a href="/add-home">Add Home</a>  <!-- link e click krle /add-home e zabe -->
-    `);
-});
+
+app.use(userRouter);  
 
 
     app.get("/add-home", (req, res, next) => {
-      console.log("Handling /add-home request", req.url, req.method);
+      console.log("Handling /add-home request", req.url, req.method);   //Handling /add-home request /add-home GET
     res.send(
     `<h1> Register your home here: </h1 >
   <form action="/submitted" method="POST">   <!-- form fill krar por /add-home & POST method e zabe -->
@@ -27,13 +19,16 @@ app.get('/', (req, res, next) => {
     `);
 });
 
+app.use(express.urlencoded());    //submit form er body dkhay. req.body er uporei hte hbe
+
 app.post("/submitted", (req, res, next) => {
-  console.log("Handling /submitted request", req.url, req.method);
+  console.log("Handling /submitted request:", req.url, req.method, req.body); //Handling /submitted request: /submitted POST { houseName: 'abcd' }
     res.send(`
        <h1>Home Registered successfully</h1>
     <a href="/">Go to Home</a>    <!-- link e click krle / e zabe -->
     `);
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
