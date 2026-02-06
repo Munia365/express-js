af258b6f2bb6cc211ed32e49ff2c38fa481bc816
Munia365 <muniasultana6826@gmail.com>
2026-02-06 11:32:21 +0600

    updated

--- src/body.js
+++ src/body.js
@@ -1,6 +1,6 @@
 // External Module
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 // app.use((req, res, next) => {
@@ -13,16 +13,21 @@ const app = express();
 //   next();
 // });
 
+// app.use((req, res, next) => {
+//   console.log("Third Middleware", req.url, req.method);
+//   res.send("<h1>Welcome to Complete Coding</h1>");
+// });
+
 app.get("/", (req, res, next) => {
-  console.log("Handling / for GET", req.url, req.method);
+  console.log("Handling / for GET", req.url, req.method);  //Handling / for GET / GET
   res.send(`<h1>Welcome to Complete Coding</h1>`);
 })
 
 app.get("/contact-us", (req, res, next) => {
-  console.log("contact-us for GET", req.url, req.method);
+  console.log("Handling /contact-us for GET", req.url, req.method);   //Handling /contact-us for GET /contact-us GET
   res.send(
     `<h1>Please give your details here</h1>
-    <form action="/submitted" method="POST">  <!-- submitted route e POST request jabe -->
+    <form action="/submitted" method="POST">
       <input type="text" name="name" placeholder="Enter your name" />
       <input type="email" name="email" placeholder="Enter your Email" />
       <input type="Submit" />
@@ -31,7 +36,15 @@ app.get("/contact-us", (req, res, next) => {
 });
 
 app.post("/submitted", (req, res, next) => {
-  console.log("submitted for POST", req.url, req.method);
+  console.log("First handling", req.url, req.method, req.body); //First handling /submitted POST undefined
+  next();
+})
+
+app.use(bodyParser.urlencoded());  //cmmnt krle undefined dkhabe
+
+app.post("/submitted", (req, res, next) => {
+  console.log("Handling /submitted for POST", req.url, req.method, req.body);
+  // Handling / submitted for POST / submitted POST { name: 'hello', email: 'ethicalcyber@gmail.com' }
   res.send(`<h1>We will contact you shortly</h1>`);
 })
 
